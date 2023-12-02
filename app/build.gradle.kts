0fbdc3eb94061bd1403ac7c7ae867ca0982df35f
ankit <ankitkamani8@gmail.com>
2023-12-02 13:56:53 +0530

    Frame Layout added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.tournamentleague"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.tournamentleague"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
