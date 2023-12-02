e838801e30a1253e2b32df73cd262da05fc7b8a1
harsh <karetiyaharsh001@gmail.com>
2023-12-02 14:22:15 +0530

    Login Page and Registration Page added

--- app/src/main/java/com/example/tournamentleague/MainActivity.kt
+++ app/src/main/java/com/example/tournamentleague/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.tournamentleague
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.tournamentleague.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavBar.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.home -> replaceFragment(HomeFragment())
+            }
+            true
+        }
+
+    }
+    private fun replaceFragment(fragment: Fragment) {
+        val myFragment = supportFragmentManager
+        val fragmentTransaction = myFragment.beginTransaction()
+        fragmentTransaction.replace(R.id.fragment, fragment)
+        fragmentTransaction.commit()
     }
 }
