4aa1c69fd5b096df5337b0c7e30984f94e5c64fa
harsh <karetiyaharsh001@gmail.com>
2023-12-08 17:33:24 +0530

    TournamentListActivity and design_bgmi_tournament_list added

---
+++ app/src/main/java/com/example/tournamentleague/Activity/TournamentListActivity.kt
@@ -0,0 +1,19 @@
+package com.example.tournamentleague.Activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tournamentleague.R
+import com.example.tournamentleague.databinding.ActivityMainBinding
+import com.example.tournamentleague.databinding.ActivityTournamentListBinding
+
+class TournamentListActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivityTournamentListBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityTournamentListBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
